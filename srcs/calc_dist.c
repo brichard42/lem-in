671cbdc2a046eb40d3b857feff43d8330daf41b5
Thomas LANDEMAINE <tlandema@e1r11p6.42.fr>
2019-07-19 07:59:34 +0200

    Once again the calculation of the distances has been changed so it will be easier to include multi-pathing in the future

--- srcs/calc_dist.c
+++ srcs/calc_dist.c
@@ -6,13 +6,13 @@
 /*   By: tlandema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 13:12:57 by tlandema          #+#    #+#             */
-/*   Updated: 2019/07/18 16:24:09 by tlandema         ###   ########.fr       */
+/*   Updated: 2019/07/19 07:57:30 by tlandema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
 
-int		ft_count_links(t_link *count, int new_hei)
+static int		ft_count_links(t_link *count, int n_hei)
 {
 	int		i;
 	t_link	*left;
@@ -25,14 +25,14 @@ int		ft_count_links(t_link *count, int new_hei)
 		return (0);
 	else
 		i++;
-	if (left && (left->l_room->hei > new_hei || left->l_room->hei == -1))
-		i = i + ft_count_links(left, new_hei);
-	if (right && (right->l_room->hei > new_hei || right->l_room->hei == -1))
-		i = i + ft_count_links(right, new_hei);
+	if (left && (left->l_room->hei > n_hei || left->l_room->hei == -1))
+		i = i + ft_count_links(left, n_hei);
+	if (right && (right->l_room->hei > n_hei || right->l_room->hei == -1))
+		i = i + ft_count_links(right, n_hei);
 	return (i);
 }
 
-int		ft_set_links(t_link *to_put, t_nod **new_tab, int i, int new_hei)
+static int		ft_set_links(t_link *to_put, t_nod **n_tab, int i, int n_hei)
 {
 	t_nod	*current_room;
 	t_link	*left;
@@ -41,72 +41,69 @@ int		ft_set_links(t_link *to_put, t_nod **new_tab, int i, int new_hei)
 	left = to_put->left;
 	right = to_put->right;
 	current_room = to_put->l_room;
-	if (to_put && (current_room->hei > new_hei || current_room->hei == -1))
-		new_tab[i++] = current_room;
+	if (to_put && (current_room->hei > n_hei || current_room->hei == -1))
+		n_tab[i++] = current_room;
 	if (left)
-		i = ft_set_links(left, new_tab, i, new_hei);
+		i = ft_set_links(left, n_tab, i, n_hei);
 	if (right)
-		i = ft_set_links(right, new_tab, i, new_hei);
+		i = ft_set_links(right, n_tab, i, n_hei);
 	return (i);
 }
 
-void	ft_get_links(t_nod **current, t_nod **new_tab, int new_hei)
+static void		ft_get_links(t_nod **curr, t_nod **n_tab, int n_hei, t_nod *end)
 {
 	int	j;
 	int	i;
-
 	
 	i = 0;
 	j = 0;
-	while (current[j])
+	while (curr[j])
 	{
-		i = ft_set_links(current[j]->links, new_tab, i, new_hei);
+		if (curr[j] != end)
+			i = ft_set_links(curr[j]->links, n_tab, i, n_hei);
 		j++;
 	}
 }
 
-static t_nod	**ft_get_current(t_nod **current, int new_hei)
+static t_nod	**ft_get_current(t_nod **curr, int n_hei, t_nod *end)
 {
 	int		i;
 	int		count;
-	t_nod	**new_tab;
+	t_nod	**n_tab;
 
 	count = 0;
 	i = 0;
-	while (current[i])
+	while (curr[i])
 	{
-		count = count + ft_count_links(current[i]->links, new_hei);
+		count = count + ft_count_links(curr[i]->links, n_hei);
 		i++;
 	}
 	if (count == 0)
-		return (ft_free_ret_nod(current));
-	if (!(new_tab = (t_nod **)ft_memalloc(sizeof(t_nod *) * (count + 1))))
-		return (ft_free_ret_nod(current));
-	ft_get_links(current, new_tab, new_hei);
-	i = 0;
-	while (new_tab[i])
-	{
-		new_tab[i]->hei = new_hei;
-		i++;
-	}
-	ft_memdel((void **)&current);
-	return (new_tab);
+		return (ft_free_ret_nod(curr));
+	if (!(n_tab = (t_nod **)ft_memalloc(sizeof(t_nod *) * (count + 1))))
+		return (ft_free_ret_nod(curr));
+	ft_get_links(curr, n_tab, n_hei, end);
+	i = -1;
+	while (n_tab[++i])
+		n_tab[i]->hei = n_hei;
+	ft_memdel((void **)&curr);
+	return (n_tab);
 }
 
-int		ft_calc_dist(t_env *env)
+int				ft_calc_dist(t_env *env)
 {
-	t_nod	**current;
+	t_nod	**curr;
 	int		i;
 
 	i = 1;
 	ft_hei_to_neg(env->tree);
 	env->start->hei = 0;
-	if (!(current = (t_nod **)ft_memalloc(sizeof(t_nod *) * 2)))
+	if (!(curr = (t_nod **)ft_memalloc(sizeof(t_nod *) * 2)))
 		return (1);
-	current[0] = env->start;
-	while (current)
+	curr[0] = env->start;
+	while (curr)
 	{
-		if (!(current = ft_get_current(current, i)))
+		if (!(curr = ft_get_current(curr, i, env->end)))
 			break ;
 		i++;
 	}
